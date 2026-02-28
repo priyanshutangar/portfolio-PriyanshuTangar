68a0347b1ef672cd9d981f7973d2cc94a633ecbf
priyanshutangar <116300269+priyanshutangar@users.noreply.github.com>
2026-03-01 02:37:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { useEffect } from 'react';
 function App() {
   const userData = {
     name: "Priyanshu Tangar",
-    designation: "Front End Developer",
+    designation: "DevOps Engineer",
     socialLinks: {
       github: "https://github.com/priyanshutangar",
       linkedin: "https://www.linkedin.com/in/priyanshu-tangar-a53686221/",
